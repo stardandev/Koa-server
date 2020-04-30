53d7d704f4d60945df4c2095a90dc7126c58e829
= <=>
2020-04-29 20:33:06 -0700

    Add KoaRouter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; // Unauthorized
-    return;
-  }
-  next().then(() => {
-    console.log('END');
-  });
-});
+router.use('/api', api.routes());
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
